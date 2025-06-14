7f37b07230ee616a4d6c620b684c9c93f61979ac
Aleksandr Goncharov <potta@inbox.ru>
2025-06-15 02:58:19 +0500

    add contacts.html, home.html. Update urls.py. Add static files.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(requests):
+    if requests.method == 'POST':
+        name = requests.POST.get('name')
+        phone = requests.POST.get('phone')
+        message = requests.POST.get('message')
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    return render(requests, 'catalog/contacts.html')
