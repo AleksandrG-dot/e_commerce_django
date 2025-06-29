e138185a6dd023f84ac97f54fc261b389b16c1a7
Aleksandr Goncharov <potta@inbox.ru>
2025-06-30 00:58:25 +0500

    Zadanie 1. Dobavilena stranica s otobrazheniem tovara i izmenen views.py, urls.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
     return render(request, 'catalog/home.html')
 
 
-def contacts(requests):
-    if requests.method == 'POST':
-        name = requests.POST.get('name')
-        phone = requests.POST.get('phone')
-        message = requests.POST.get('message')
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(requests, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except:
+        return HttpResponse(f"Товар с номером ID {product_id} не найден!")
+    context = {'product_id': product_id,'product': product}
+    return render(request, 'catalog/product_detail.html', context)
