965d8c4326d7991fe3343045495d04b16af7c7c7
Aleksandr Goncharov <potta@inbox.ru>
2025-07-10 01:53:37 +0500

    Zadanie 3. Modifikaciya vvoda i obraotki zaorosov

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -17,13 +17,24 @@ class BlogUpdateView(UpdateView):
     fields = ["title", "content", "preview", "is_published"]
     success_url = reverse_lazy('blog:blog_list')
 
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 class BlogListView(ListView):
     model = BlogPost
 
+    def get_queryset(self):
+        return BlogPost.objects.filter(is_published=True)
+
 class BlogDetailView(DetailView):
     model = BlogPost
     context_object_name = 'blog'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
 
 
 class BlogDeleteView(DeleteView):
