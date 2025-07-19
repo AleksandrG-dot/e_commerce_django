5fdb35d4e70b72efdf922eb22ab9ef9c3c9c171d
Aleksandr Goncharov <potta@inbox.ru>
2025-07-19 20:11:28 +0500

    ydalenie polya username iz modeli. okazalos chto ego ne nado v formah

--- users/models.py
+++ users/models.py
@@ -4,13 +4,14 @@ from django_countries.fields import CountryField
 
 
 class UserModel(AbstractUser):
+    username = None
     email = models.EmailField(unique=True, verbose_name="e-mail")
     phone = models.CharField(max_length=15, blank=True, null=True, verbose_name='Номер телефона')
     country = CountryField(default='ru', blank=False, null=False, blank_label='Выберите страну')
     avatar = models.ImageField(upload_to='Users/avatars/', blank=True, null=True, verbose_name='Аватар')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', ]
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f'{self.username}  e-mail: {self.email}  country: {self.country}  phone: {self.phone_number}'
@@ -18,4 +19,3 @@ class UserModel(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-        ordering = ['username']
